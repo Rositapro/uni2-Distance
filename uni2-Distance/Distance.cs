a2c003454bdbcbc8b405a497c039165985bcc956
abrah <abrah@HPDEABRAHAM>
2024-02-19 23:39:31 -0600

    Agregar archivos de proyecto.

---
+++ uni2-Distance/Distance.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uni2_Distance
+{
+    internal class Distance
+    {
+        double speed;
+        double time;
+        double distance;
+
+        //constructor
+        public Distance (double speed, double time)
+        {
+            this.speed = speed;
+            this.time = time;
+            distance = 0;
+            operacion();
+        }
+        public override string ToString()
+        {
+            return "the distance is: " + distance.ToString();
+        }
+
+        public void operacion()
+        {
+            distance = (speed * time);
+        }
+
+    }
+}
+
---
+++ uni2-Distance/Program.cs
@@ -0,0 +1,9 @@
+﻿
+using uni2_Distance;
+
+Console.WriteLine("enter the speed:");
+double sleep = double.Parse(Console.ReadLine());
+Console.WriteLine("enter the time:");
+double time = double.Parse(Console.ReadLine());
+Distance distance = new Distance(sleep, time);
+Console.WriteLine(distance.ToString());
